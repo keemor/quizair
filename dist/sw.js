e4991bef8b9342b01428b031bb447f8ddea9a172
keemor <keemor@keemor.com>
2018-07-25 15:58:54 +0200

    sw-cache support

---
+++ dist/sw.js
@@ -0,0 +1,53 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "client.0f7457be.js",
+    "revision": "4f31b9fd598b9f7cc1e6d98dc790880d"
+  },
+  {
+    "url": "client.86b826db.css",
+    "revision": "429e48b24921a95eebbcd0ff6f8695d4"
+  },
+  {
+    "url": "icon.0fc16281.png",
+    "revision": "ddb36181eb772d1b2992401f536630d1"
+  },
+  {
+    "url": "icon512.ea8be215.png",
+    "revision": "e3f46e824f666a483004e4d705c3c1aa"
+  },
+  {
+    "url": "index.html",
+    "revision": "3abaf5db409bb379b6f8ce087cb7cfb3"
+  },
+  {
+    "url": "/",
+    "revision": "512d2d344bad52b8421868e2c1e06f1f"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/index.html");
