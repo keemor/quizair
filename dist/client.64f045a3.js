67bc8436827aad1be19ccc5ee3e9bb194c537dc4
keemor <keemor@keemor.com>
2018-07-25 15:19:01 +0200

    Offline support

--- dist/client.64f045a3.js
+++ dist/client.64f045a3.js
@@ -3560,7 +3560,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '57923' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '62556' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
